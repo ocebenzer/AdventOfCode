990c505ed5c4b490bfa82563343f71ad1c87b890
Omer Benzer <omer.benzer@tum.de>
2023-12-01 17:56:30 +0100

    day1p2

--- day1.py
+++ day1.py
@@ -1,12 +1,32 @@
-import sys
+import re
 from functools import reduce
 
-lines = []
+forward_list = [
+    r"\d",
+    r"one",
+    r"two",
+    r"three",
+    r"four",
+    r"five",
+    r"six",
+    r"seven",
+    r"eight",
+    r"nine"
+]
 
-for line in sys.stdin:
-    if line == "done\n": break
-    lines.append([int(char) for char in line if char.isdigit()])
+backward_list = list(map(lambda str: str[::-1], forward_list))
+backward_list[0] = r"\d"
 
-numbers = list(map(lambda chars: chars[0] * 10 + chars[-1], lines))
+forward = re.compile("|".join(forward_list))
+backward = re.compile("|".join(backward_list))
 
-print(reduce(lambda a, b: a + b, numbers))
+total = 0
+
+for line in open("day1.txt").readlines():
+    first_match = forward.search(line).group()
+    first = int(first_match) if first_match.isdigit() else forward_list.index(first_match)
+    last_match = backward.search(line[::-1]).group()
+    last = int(last_match) if last_match.isdigit() else backward_list.index(last_match)
+    total += int(first) * 10 + int(last)
+
+print(total)
