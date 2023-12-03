d82ed1a530d39abf5fbf0404d06e62fb1bd1feb9
Omer Benzer <omer.benzer@tum.de>
2023-12-03 14:42:06 +0100

    day3p2

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -7,6 +7,13 @@
 
 const auto is_digit = [] (const char c) { return c >= '0' && c <= '9'; };
 const auto is_symbol = [] (const char c) { return c < '0' && c > '9' || c != '.'; };
+const auto is_gear = [] (const char c) { return c == '*'; };
+
+struct number {
+    int row;
+    int col;
+    int value;
+};
 
 int main() {
     std::ifstream file{"../day3.txt"};
@@ -27,45 +34,47 @@ int main() {
 
     for (int row{0}; row < lines.size(); row++) {
         const std::string_view line{lines[row]};
-        for (int number_begin{1}; number_begin < line.size(); number_begin++) {
-            if (is_digit(line[number_begin-1]) || !is_digit(line[number_begin])) continue;
+        for (int col{0}; col < line.size(); col++) {
+            if (!is_gear(line[col])) continue;
 
-            int number = std::atoi(&line[number_begin]);
-
-            int number_end{number_begin};
-            while (number_end < line.size() && is_digit(line[number_end])) {
-                number_end++;
-            }
-
-            bool is_part_number = false;
-
-            const auto has_symbol = [] (const std::string_view& line) {
-                for (const char c : line) {
-                    if (is_symbol(c)) return true;
+            const auto get_number = [] (const std::string_view& line, int row, int col) {
+                if (!is_digit(line[col])) {
+                    return number{row, col, -1};
                 }
-                return false;
+                while (is_digit(line[col])) {
+                    col--;
+                }
+                return number{row, col+1, std::atoi(&line[col+1])};
             };
+
+            std::vector<number> numbers;
 
             if (row > 0) {
                 const auto upper_line = std::string_view{lines[row-1]};
-                const auto upper_line_slice = upper_line.substr(number_begin-1, number_end - number_begin + 2);
-                if (has_symbol(upper_line_slice)) {
-                    sum += number;
-                    continue;
-                }
+                
+                numbers.push_back(get_number(upper_line, row-1, col-1));
+                numbers.push_back(get_number(upper_line, row-1, col));
+                numbers.push_back(get_number(upper_line, row-1, col+1));
             }
 
-            if (is_symbol(line[number_begin - 1]) || is_symbol(line[number_end])) {
-                    sum += number;
-                    continue;
-            }
+            numbers.push_back(get_number(line, row, col-1));
+            numbers.push_back(get_number(line, row, col+1));
 
             if (row < lines.size()-1) {
                 const auto lower_line = std::string_view{lines[row+1]};
-                const auto lower_line_slice = lower_line.substr(number_begin-1, number_end - number_begin + 2);
-                if (has_symbol(lower_line_slice)) {
-                    sum += number;
-                    continue;
+
+                numbers.push_back(get_number(lower_line, row+1, col-1));
+                numbers.push_back(get_number(lower_line, row+1, col));
+                numbers.push_back(get_number(lower_line, row+1, col+1));
+            }
+
+            int n_numbers = std::erase_if(numbers, [] (const number& n) { return n.value == -1; });
+
+            const auto& n1 = numbers[0];
+            for (const auto& n2 : numbers) {
+                if (n1.row != n2.row || n1.col != n2.col) {
+                    sum += n1.value * n2.value;
+                    break;
                 }
             }
         }
