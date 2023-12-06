3d46c6b13387c3de5f572e1084ae85c6a1b42430
Omer Benzer <omer.benzer@tum.de>
2023-12-06 11:32:10 +0100

    day6p2

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -1,14 +1,13 @@
+#include <cstdlib>
 #include <fstream>
 #include <sstream>
 #include <iostream>
-#include <vector>
 #include <string>
-#include <utility>
-#include <numeric>
 
 int main() {
-    std::vector<std::pair<int, int>> races;
-    std::vector<long> ways;
+    std::string s_time{""};
+    std::string s_distance{""};
+    long ways{0};
     {
         std::ifstream file{"../day6.txt"};
         if (!file.is_open()) {
@@ -25,9 +24,8 @@ int main() {
             ss >> token;
 
             while(!ss.eof()) {
-                long time;
-                ss >> time;
-                races.push_back(std::make_pair(time, 0));
+                ss >> token;
+                s_time += token;
             }
         }
         {
@@ -37,40 +35,37 @@ int main() {
             ss >> token;
 
             for(int i{0}; !ss.eof(); i++) {
-                long distance;
-                ss >> distance;
-                races[i].second = distance;
+                ss >> token;
+                s_distance += token;
             }
         }
     }
 
-    for (const auto& [time, distance] : races) {
+    {
+        long long time{std::atoll(s_time.c_str())};
+        long long distance{std::atoll(s_distance.c_str())};
         const auto calculate_distance = [] (long button_hold_time, long travel_time) {
             return button_hold_time * travel_time;
         };
 
-        long ctr{0};
-
         if (time % 2 == 0) {
             if (calculate_distance(time/2, time/2) > distance) {
-                ctr++;
+                ways++;
             }
             else {
-                ways.push_back(0);
-                continue;
+                std::cout << 0 << std::endl;
+                exit(0);
             }
         }
 
-        const long middle{(time-1) / 2};
+        const auto middle{(time-1) / 2};
 
-        long button_hold_time{middle};
+        auto button_hold_time{middle};
         while (calculate_distance(button_hold_time, time - button_hold_time) > distance) {
-            ctr += 2;
+            ways += 2;
             button_hold_time--;
         }
-
-        ways.push_back(ctr);
     }
 
-    std::cout << std::reduce(ways.begin(), ways.end(), 1, [] (long a, long b) { return a*b; }) << std::endl;
+    std::cout << ways << std::endl;
 }
