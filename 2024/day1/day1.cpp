0b30bb2a636703f305135ff39455c5c6743d7e98
omer <omer@omer.com>
2024-12-06 21:51:44 +0100

    add day1 part2

--- 2024/day1/day1.cpp
+++ 2024/day1/day1.cpp
@@ -2,27 +2,32 @@
 
 #include "utils.h"
 #include <ranges>
+#include <unordered_map>
 
 int main() {
     const auto lines = ocb::read_lines("day1/input1.txt");
 
-    std::vector<int> lefts, rights;
+    std::vector<int> lefts;
+    std::unordered_map<int, int> rights;
     for (const auto& line : lines) {
         std::stringstream ss(line);
         int left, right;
         ss >> left >> right;
         lefts.push_back(left);
-        rights.push_back(right);
+
+        if (rights.contains(right)) {
+            ++rights[right];
+        }
+        else {
+            rights[right] = 1;
+        }
     }
 
     std::ranges::sort(lefts);
-    std::ranges::sort(rights);
 
     std::vector<int> result;
-    for (int i = 0; i < lefts.size(); ++i) { // std::ranges::zip_view my beloved
-        const auto& left = lefts[i];
-        const auto& right = rights[i];
-        result.push_back(std::abs(left - right));
+    for (const auto& left : lefts) { // std::ranges::zip_view my beloved
+        result.push_back(left * rights[left]);
     }
 
     const auto sum = std::accumulate(result.begin(), result.end(), 0);
