fb7cd60aae27e5f35c7107388965480812edfcd7
omer <omer@omer.com>
2024-12-06 23:49:22 +0100

    add day4 part2

--- 2024/day4/day4.cpp
+++ 2024/day4/day4.cpp
@@ -10,55 +10,35 @@ int main() {
     for (int row = 0; row < lines.size(); row++) {
         for (int col = 0; col < lines.size(); col++) {
             const auto& x = lines[row][col];
-            if (x != 'X') continue;
+            if (x != 'A') continue;
 
             try {
-                const auto& m = lines.at(row).at(col+1);
-                const auto& a = lines.at(row).at(col+2);
-                const auto& s = lines.at(row).at(col+3);
-                if (m == 'M' && a == 'A' && s == 'S') counter++;
+                const auto& m1 = lines.at(row-1).at(col-1);
+                const auto& s1 = lines.at(row+1).at(col+1);
+                const auto& m2 = lines.at(row+1).at(col-1);
+                const auto& s2 = lines.at(row-1).at(col+1);
+                if (m1 == 'M' && m2 == 'M' && s1 == 'S' && s2 == 'S') counter++;
             } catch (const std::out_of_range& e) { }
             try {
-                const auto& m = lines.at(row+1).at(col+1);
-                const auto& a = lines.at(row+2).at(col+2);
-                const auto& s = lines.at(row+3).at(col+3);
-                if (m == 'M' && a == 'A' && s == 'S') counter++;
+                const auto& m1 = lines.at(row+1).at(col+1);
+                const auto& s1 = lines.at(row-1).at(col-1);
+                const auto& m2 = lines.at(row+1).at(col-1);
+                const auto& s2 = lines.at(row-1).at(col+1);
+                if (m1 == 'M' && m2 == 'M' && s1 == 'S' && s2 == 'S') counter++;
             } catch (const std::out_of_range& e) { }
             try {
-                const auto& m = lines.at(row+1).at(col);
-                const auto& a = lines.at(row+2).at(col);
-                const auto& s = lines.at(row+3).at(col);
-                if (m == 'M' && a == 'A' && s == 'S') counter++;
+                const auto& m1 = lines.at(row-1).at(col-1);
+                const auto& s1 = lines.at(row+1).at(col+1);
+                const auto& m2 = lines.at(row-1).at(col+1);
+                const auto& s2 = lines.at(row+1).at(col-1);
+                if (m1 == 'M' && m2 == 'M' && s1 == 'S' && s2 == 'S') counter++;
             } catch (const std::out_of_range& e) { }
             try {
-                const auto& m = lines.at(row+1).at(col-1);
-                const auto& a = lines.at(row+2).at(col-2);
-                const auto& s = lines.at(row+3).at(col-3);
-                if (m == 'M' && a == 'A' && s == 'S') counter++;
-            } catch (const std::out_of_range& e) { }
-            try {
-                const auto& m = lines.at(row).at(col-1);
-                const auto& a = lines.at(row).at(col-2);
-                const auto& s = lines.at(row).at(col-3);
-                if (m == 'M' && a == 'A' && s == 'S') counter++;
-            } catch (const std::out_of_range& e) { }
-            try {
-                const auto& m = lines.at(row-1).at(col-1);
-                const auto& a = lines.at(row-2).at(col-2);
-                const auto& s = lines.at(row-3).at(col-3);
-                if (m == 'M' && a == 'A' && s == 'S') counter++;
-            } catch (const std::out_of_range& e) { }
-            try {
-                const auto& m = lines.at(row-1).at(col);
-                const auto& a = lines.at(row-2).at(col);
-                const auto& s = lines.at(row-3).at(col);
-                if (m == 'M' && a == 'A' && s == 'S') counter++;
-            } catch (const std::out_of_range& e) { }
-            try {
-                const auto& m = lines.at(row-1).at(col+1);
-                const auto& a = lines.at(row-2).at(col+2);
-                const auto& s = lines.at(row-3).at(col+3);
-                if (m == 'M' && a == 'A' && s == 'S') counter++;
+                const auto& m1 = lines.at(row+1).at(col+1);
+                const auto& s1 = lines.at(row-1).at(col-1);
+                const auto& m2 = lines.at(row-1).at(col+1);
+                const auto& s2 = lines.at(row+1).at(col-1);
+                if (m1 == 'M' && m2 == 'M' && s1 == 'S' && s2 == 'S') counter++;
             } catch (const std::out_of_range& e) { }
         }
     }
