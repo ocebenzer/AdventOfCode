27eba62eacb52f6b372dc9fb698c46db80f8331c
omer <omer@omer.com>
2024-12-06 23:21:37 +0100

    add day3 part2

--- 2024/day3/day3.cpp
+++ 2024/day3/day3.cpp
@@ -2,14 +2,14 @@
 
 #include "utils.h"
 
+const std::regex mul_regex(R"(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))");
+
 int main() {
     const auto lines = ocb::read_lines("day3/input1.txt");
 
-    const std::regex mul_regex(R"(mul\(\d{1,3},\d{1,3}\))");
-    std::smatch results;
-
     std::vector<std::string> valid_commands;
     for (const auto& line : lines) {
+        std::smatch results;
         for (auto line_iter = line.begin();
             std::regex_search(line_iter, line.end(), results, mul_regex);
             line_iter = results[0].second) {
@@ -18,12 +18,21 @@ int main() {
     }
 
     int sum = 0;
+    bool disabled = false;
     for (auto& command : valid_commands) {
-        const auto formatted_command = regex_replace(command, std::regex("\\D"), " ");
-        std::stringstream ss(formatted_command);
-        int n1=0, n2=0;
-        ss >> n1 >> n2;
-        sum += n1 * n2;
+        if (command == "do()") {
+            disabled = false;
+        }
+        else if (command == "don't()") {
+            disabled = true;
+        }
+        else if (!disabled) {
+            const auto formatted_command = regex_replace(command, std::regex("\\D"), " ");
+            std::stringstream ss(formatted_command);
+            int n1=0, n2=0;
+            ss >> n1 >> n2;
+            sum += n1 * n2;
+        }
     }
 
     std::cout << sum << std::endl;
