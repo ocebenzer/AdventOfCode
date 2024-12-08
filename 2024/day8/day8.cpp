d41af6db9c12a98da4638b595bff3d8bbb100f0c
omer <omer@omer.com>
2024-12-08 11:56:41 +0100

    add day8 part2

--- 2024/day8/day8.cpp
+++ 2024/day8/day8.cpp
@@ -7,6 +7,11 @@
 int main() {
     const auto lines = ocb::read_lines("day8/input1.txt");
 
+    const auto in_grid = [&lines] (const auto node) {
+        return node.col >= 0 && node.col < lines.size() &&
+            node.row >= 0 && node.row < lines[0].size();
+    };
+
     std::unordered_map<char, std::vector<ocb::Position>> node_lists;
 
     for (int row = 0; row < lines.size(); row++) {
@@ -26,9 +31,9 @@ int main() {
         }
     }
 
-
-    std::unordered_map<char, std::vector<ocb::Position>> antinode_lists;
-    for (const auto& [c, node_list] : node_lists) {
+    std::unordered_set<ocb::Position> unique_antinodes;
+    for (const auto& [antenna, node_list] : node_lists) {
+        auto lines_copy = lines;
         for (auto i = 0; i < node_list.size(); ++i) {
             for (int j = 0; j < node_list.size(); ++j) {
                 if (j == i) continue;
@@ -36,29 +41,23 @@ int main() {
                 const auto& node1 = node_list[i];
                 const auto& node2 = node_list[j];
 
-                const ocb::Position antinode{
-                    node2.row + (node2.row - node1.row),
-                    node2.col + (node2.col - node1.col),
+                const ocb::Position antinode_vector{
+                    (node2.row - node1.row),
+                    (node2.col - node1.col),
                 };
 
-                if (antinode_lists.contains(c)) {
-                    auto& antinode_list = antinode_lists[c];
-                    antinode_list.push_back(antinode);
+                for (auto antinode = node2; in_grid(antinode); antinode += antinode_vector) {
+                    lines_copy[antinode.row][antinode.col] = '#';
                 }
-                else {
-                    std::vector<ocb::Position> antinode_list;
-                    antinode_list.emplace_back(antinode);
-                    antinode_lists.emplace(c, antinode_list);
-                }
-
             }
         }
-    }
-
-    std::unordered_set<ocb::Position> unique_antinodes;
-    for (const auto& [c, node_list] : antinode_lists) {
-        for (const auto& node : node_list) {
-            unique_antinodes.insert(node);
+        for (int row = 0; row < lines_copy.size(); row++) {
+            for (int col = 0; col < lines_copy[row].size(); col++) {
+                const auto& c = lines_copy[row][col];
+                if (c == '#') {
+                    unique_antinodes.emplace(row, col);
+                }
+            }
         }
     }
 
